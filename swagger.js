8022c5bf9c7c72178d09f964d968da9a5fe20507
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2023-10-09 15:44:38 -0600

    created swagger files and installed swaggerautogen

---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc={
+    info:{
+        title:'Game Review API',
+        description: 'Collects and Displays Video Game Review Information'
+    },
+    host:'localhost:8000',
+    schemes: ['http']
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./routes/index.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
