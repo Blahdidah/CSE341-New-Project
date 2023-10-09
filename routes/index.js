05e067262c6ba6e05d4dad73579cac87e59d1940
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2023-10-09 15:55:17 -0600

    requiring swagger in routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 //this file will have my endpoints
 const express = require('express');
 const router = express.Router();
+router.use('/', require('./swagger'));
 
 //router.use('/', require('./swagger'));
 router.use('/reviews', require('./reviews'))
